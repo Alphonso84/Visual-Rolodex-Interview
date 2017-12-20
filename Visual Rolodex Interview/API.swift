9d5084d08c39de64c0b6f52f5e5fb002e38b677c
Alphonso Bret Sensley II <fonzman1984@me.com>
2017-12-20 14:58:44 -0800

    CodeBase: Update code eliminate needed code from other projects finish get request for rolodexUserData

--- Visual Rolodex Interview/API.swift
+++ Visual Rolodex Interview/API.swift
@@ -10,49 +10,21 @@ import Foundation
 class API: ViewController {
     
    
-    
-    
-    
-    func buildUrl(constructedUrl: String) -> URL{
-        let baseURL = ""
-        //URL FILTER LIMITS RESULTS RETURNED
-        let numberOfResults = "&$limit=20"
-        //URL FILTER ORDERS BY MOST RECENT
-        let sortOrder = "&$order=datetime DESC"
-        //URL FILTER ORDERS RESULTS BY CITY(GLOBAL VARIABLE)
-        let userCity = "&city=\(city)"
-        // print(MainViewController.CityString().city)
-        
-        urlString = "\(baseURL)\(token)\(userCity)\(sortOrder)\(numberOfResults)"
-        
-        let FormattedUrlString = urlString.replacingOccurrences(of: " ", with: "%20")
-        let url = URL(string: FormattedUrlString)
-        return url!
-        
-    }
-    
-
-    
-    
     func parseJSON() {
-        let unwrappedURL = buildUrl(constructedUrl: urlString)
+        let baseURL = "https://s3-us-west-2.amazonaws.com/udacity-mobile-interview/CardData.json"
+         let dataForURL = URL(string: baseURL)
+        
         //URL SESSION
         let session = URLSession.shared
-        let task = session.dataTask(with: (unwrappedURL)) { (data, response, error) in
+        let task = session.dataTask(with: (dataForURL)!) { (data, response, error) in
             print("Start")
             guard let unwrappedData = data else {return}
             do {
                 
                 let jsonData = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as? [[String:Any]]
                 //JSONDATA IS AN ARRAY OF DICTIONARIES
-                RolodexUsers = jsonData!
-                
-                // ACESSING THE ARRAY AND CREATING DICTIONARY INSTANCES (The first dictionary is the most recent by default)
-                
-                
-                //CREATING STRING INSTANCES OF DICTIONARY VALUES
-                
-                
+               let RolodexUsers = jsonData!
+                print(RolodexUsers)
                 
             } catch {
                 print(error)
